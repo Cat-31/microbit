d2ea1cf8096092015ef3af0719e09bcb2607a2b5
Week9nd <maoxianfeng@gmail.com>
2020-03-12 20:13:08 +0800

    add demo

--- python/01/demo01_display.py
+++ python/01/demo01_display.py
@@ -1,13 +1,6 @@
+
 # 导入模块
 from microbit import display
-from microbit import sleep
 
-# 显示1
+display.show('Hello World')
 display.show(1)
-
-# 等待1秒
-sleep(1000)
-display.show(2)
-
-sleep(1000)
-display.show(3)
